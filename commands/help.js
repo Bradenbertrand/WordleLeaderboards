824cf861f56fd11f6b14f7410da434f125b8d2a2
Bradenbertrand <46539405+Bradenbertrand@users.noreply.github.com>
2022-02-04 12:54:26 -0500

    Update help.js
    Updated to include !wltopall command

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,5 @@
 module.exports.run = async(bot, message, args) => {
-    message.channel.send("Commands:\n!wlhelp - this command\n!wlsave <Pasted Wordle Score> - add your Wordle score \n!wltop - See the top 5 players!\n!wlavg - See your average score")
+    message.channel.send("Commands:\n!wlhelp - this command\n!wlsave <Pasted Wordle Score> - add your Wordle score \n!wltop - See the top 5 players on this server\n!wltopall - See the top 5 players on all servers\n!wlavg - See your average score")
 }
 
 module.exports.config = {
