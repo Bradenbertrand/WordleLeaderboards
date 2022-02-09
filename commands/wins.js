1354653c5344613251e40e34b56f2d4c193a7391
Bradenbertrand <46539405+Bradenbertrand@users.noreply.github.com>
2022-02-09 15:31:01 -0500

    Added a command
    Added a wins command to see your wins and losses

---
+++ commands/wins.js
@@ -0,0 +1,27 @@
+const User = require('../models/user');
+
+module.exports.run = async (bot, message, args) => {
+    let userid = message.author.id
+    //Finds a user based on userId
+    User.findOne({ userId: userid}, function (err, currentUser) {
+        if (err) {
+            console.log("Theres been an error finding a user")
+            message.channel.send("Theres been an error contact big daddy braden pls :)")
+        }
+        //If user does not exist, send a error. This should never happen.
+        if (!currentUser) {
+            console.log("User does not exist")
+            message.channel.send("You don't exist or something. That's pretty weird.")
+        } else {
+            //Send a message with the username and average score of the message sender.
+            message.channel.send("User " +
+            message.author.username + " has won " + 
+            currentUser.wins + " games and lost " +
+            currentUser.losses + ", with a win rate of " + currentUser.winRate + "%.");
+        }
+    })
+}
+
+module.exports.config = {
+    command: "wins"
+}
