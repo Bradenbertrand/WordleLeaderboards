012ed2acead5b46f0751878db90118e80330c6d3
Bradenbertrand <46539405+Bradenbertrand@users.noreply.github.com>
2022-02-12 13:41:43 -0500

    Update Help.js
    Added new commands

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,12 @@
 module.exports.run = async(bot, message, args) => {
-    message.channel.send("Commands:\n!wlhelp - this command\n!wlsave <Pasted Wordle Score> - add your Wordle score \n!wltop - See the top 5 players on this server\n!wltopall - See the top 5 players on all servers\n!wlavg - See your average score")
+    message.channel.send(`Commands:
+    !wlhelp - this command
+    !wlsave <Pasted Wordle Score> - add your Wordle score 
+    !wltop - See the top 5 players on this server
+    !wltopall - See the top 5 players on all servers
+    !wlavg - See your average score
+    !wlpoints - See your current points
+    !wlwins - See your wins, losses, and win rate`)
 }
 
 module.exports.config = {
