54cbb0a11ef98f622655fba58c251e476b0cea6d
Bradenbertrand <46539405+Bradenbertrand@users.noreply.github.com>
2022-02-13 14:39:38 -0500

    Update top
    Updated default leaderboard, added arguments to top for specifying which leaderboard you want to see.

--- commands/help.js
+++ commands/help.js
@@ -3,6 +3,9 @@ module.exports.run = async(bot, message, args) => {
     !wlhelp - this command
     !wlsave <Pasted Wordle Score> - add your Wordle score 
     !wltop - See the top 5 players on this server
+    !wltop all - see the top 5 players on all servers
+    !wltop avg - See the top 5 player averages on this server
+    !wltop avg all - See the top 5 player averages on all servers
     !wltopall - See the top 5 players on all servers
     !wlavg - See your average score
     !wlpoints - See your current points
--- commands/top.js
+++ commands/top.js
@@ -3,6 +3,42 @@ const User = require('../models/user');
 module.exports.run = async (bot, message, args) => {
     if (args == "") {
         //Creates an array based on users in the current server, with a score higher than 0.1, with a limit of 5 users.
+        const usersSorted = await User.find({ servers: message.guild.id}).sort({ points: 1 }).limit(5);
+        try {
+            var i = 1
+            var returnedScores = `Top users for this server:\n`;
+            //Create a line of text for each user
+            usersSorted.forEach((user) => {
+                returnedScores += `#${i} - ${user.username} with ${user.points} points\n`
+                i += 1
+            })
+            //Returns the created string
+            message.channel.send(returnedScores);
+            console.log("Successfully put out scores")
+        } catch (err) {
+            console.log(err)
+            message.channel.send("There has been an error! This is probably due to less than 5 people having scores on your server.")
+        }
+    } else if (args == "all") {
+        //Creates an array based on users in the current server, with a score higher than 0.1, with a limit of 5 users.
+        const usersSorted = await User.find({}).sort({ points: 1 }).limit(5);
+        try {
+            var i = 1
+            var returnedScores = `Top users for this server:\n`;
+            //Create a line of text for each user
+            usersSorted.forEach((user) => {
+                returnedScores += `#${i} - ${user.username} with ${user.points} points\n`
+                i += 1
+            })
+            //Returns the created string
+            message.channel.send(returnedScores);
+            console.log("Successfully put out scores")
+        } catch (err) {
+            console.log(err)
+            message.channel.send("There has been an error! This is probably due to less than 5 people having scores on your server.")
+        }
+    } else if (args == "avg") {
+        //Creates an array based on users in the current server, with a score higher than 0.1, with a limit of 5 users.
         const usersSorted = await User.find({ servers: message.guild.id, scoreAvg: { $gte: 0.1 }, gamesPlayed: { $gte: 2 } }).sort({ scoreAvg: 1 }).limit(5);
         try {
             var i = 1
@@ -19,7 +55,7 @@ module.exports.run = async (bot, message, args) => {
             console.log(err)
             message.channel.send("There has been an error! This is probably due to less than 5 people having scores on your server.")
         }
-    } else if (args == "all") {
+    } else if (args == "avg all") {
         //Creates an array based on all users in the current server, with a score higher than 0.1, with a limit of 5 users.
         const usersSorted = await User.find({ scoreAvg: { $gte: 0.1 }, gamesPlayed: { $gte: 2 } }).sort({ scoreAvg: 1 }).limit(5);
         try {
