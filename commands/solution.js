0766f4c00da9df11bd57b2e8ba13553aecc01969
Bradenbertrand <46539405+Bradenbertrand@users.noreply.github.com>
2022-02-14 16:02:41 -0500

    Update solution.js

--- commands/solution.js
+++ commands/solution.js
@@ -1,9 +1,11 @@
 const axios = require('axios')
 
 module.exports.run = async (bot, message, args) => {
+    const date = new Date();
     if (args == "") {
-        axios.get('https://wordle-solutions.herokuapp.com/').then(function (solution) {
+        axios.get(`https://wordle-solutions.herokuapp.com/2022-${date.getMonth()}-${date.getDay()}}`).then(function (solution) {
             console.log(solution)
+            console.log
             message.channel.send("beep bop ba doop doop");
         })
     }
