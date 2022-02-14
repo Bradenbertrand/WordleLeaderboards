ca3408370a0cfeac13094b0bec7e77d47138ebcb
Bradenbertrand <46539405+Bradenbertrand@users.noreply.github.com>
2022-02-14 15:20:04 -0500

    Update solution.js
    Trying to fix json parsing

--- commands/solution.js
+++ commands/solution.js
@@ -3,8 +3,8 @@ const axios = require('axios')
 module.exports.run = async (bot, message, args) => {
     if (args == "") {
         axios.get('https://wordle-solutions.herokuapp.com/').then(function (solution) {
-            const solutionParsed = JSON.parse(solution)
-            message.channel.send(solutionParsed);
+            console.log(solution)
+            message.channel.send("beep bop ba doop doop");
         })
     }
 }
