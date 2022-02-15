5498fe5f1b4d41a8b59a51ad3435702962dd9955
Bradenbertrand <46539405+Bradenbertrand@users.noreply.github.com>
2022-02-15 12:48:06 -0500

    Update solution.js
    Attempt to fix

--- commands/solution.js
+++ commands/solution.js
@@ -2,11 +2,11 @@ const axios = require('axios')
 
 module.exports.run = async (bot, message, args) => {
     const date = new Date();
+    const todaysDate = `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`
     if (args == "") {
-        axios.get(`https://wordle-solutions.herokuapp.com/2022-${date.getMonth()}-${date.getDay()}}`).then(function (solution) {
-            console.log(solution)
-            console.log
-            message.channel.send("beep bop ba doop doop");
+        axios.get(`https://wordle-solutions.herokuapp.com/`).then(function (solution) {
+            console.log(solution.todaysDate)
+            message.channel.send(solution.todaysDate);
         })
     }
 }
