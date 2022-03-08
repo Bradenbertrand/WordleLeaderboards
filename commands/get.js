02eadf7ea8cac2a69050e4bab1ba3f78b6d69a63
Bradenbertrand <46539405+Bradenbertrand@users.noreply.github.com>
2022-03-08 13:03:43 -0500

    Update get.js
    Potentially functional - still in dev mode

--- commands/get.js
+++ commands/get.js
@@ -1,9 +1,17 @@
 const Score = require('../models/score');
 module.exports.run = async (bot, message, args) => {
-    let userId = message.author.id.slice(0, 10);
+    let userid = message.author.id.slice(0, 10);
     let scoreid = args;
     let scores = await Score.find({});
     console.log(scores.length);
+
+    let sortedArray = scores.filter(function (score) {
+        return score.userId.slice(0,10) = userid
+    }).filter(function (score) {
+        return score.wordleNumber = scoreid
+    })
+
+    console.log(sortedArray)
 }
 
 module.exports.config = {
