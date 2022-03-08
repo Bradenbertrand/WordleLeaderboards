5444467badcbae33eef3e90c337038e9cbf5901f
Bradenbertrand <46539405+Bradenbertrand@users.noreply.github.com>
2022-03-08 15:02:48 -0500

    Update get.js
    Fixed extra space in message

--- commands/get.js
+++ commands/get.js
@@ -14,8 +14,7 @@ module.exports.run = async (bot, message, args) => {
     if (sortedArray.length == 0) {
         message.channel.send("No score found. Please try again with a valid 3 digit game")
     } else {
-        message.channel.send(`Wordle ${scoreid} ${sortedArray[0].score}/6
-        ${sortedArray[0].scorePattern}`)
+        message.channel.send(`Wordle ${scoreid} ${sortedArray[0].score}/6 \n${sortedArray[0].scorePattern}`)
     }
 }
 
