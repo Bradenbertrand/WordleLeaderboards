5477264f658b5fafe6c3e464b370019a64137f3f
Bradenbertrand <46539405+Bradenbertrand@users.noreply.github.com>
2022-03-08 15:05:30 -0500

    Update get.js
    Removed un needed console log

--- commands/get.js
+++ commands/get.js
@@ -3,7 +3,6 @@ module.exports.run = async (bot, message, args) => {
     let userid = message.author.id.slice(0, 10);
     let scoreid = args;
     let scores = await Score.find({});
-    console.log(scores.length);
 
     let sortedArray = scores.filter(function (score) {
         return score.userId.slice(0,10) == userid
