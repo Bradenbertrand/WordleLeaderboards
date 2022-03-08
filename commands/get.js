1c8a6bff0b9d4af3c1d41803a936d90d75710960
Bradenbertrand <46539405+Bradenbertrand@users.noreply.github.com>
2022-03-08 12:28:57 -0500

    Update get.js

--- commands/get.js
+++ commands/get.js
@@ -2,7 +2,7 @@ const Score = require('../models/score');
 module.exports.run = async (bot, message, args) => {
     let userId = message.author.id.slice(0, 10);
     let scoreid = args;
-    let scores = Score.find({});
+    let scores = await Score.find({});
     console.log(scores);
 }
 
