eed5a7d45921c0bcc0d209b487b2d48fcfb17c0f
Bradenbertrand <46539405+Bradenbertrand@users.noreply.github.com>
2022-03-09 15:39:51 -0500

    Update get.js

--- commands/get.js
+++ commands/get.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+var readline = require('readline');
 const Score = require('../models/score');
 const getSolution = require('../helpers/getSolution');
 
@@ -5,6 +7,25 @@ module.exports.run = async (bot, message, args) => {
     let userid = message.author.id.slice(0, 10);
     let scoreid = args;
     let scores = await Score.find({});
+    let x = 0;
+    var filename = 'Solutions.txt';
+
+
+    const solution = readline.createInterface({
+        input: fs.createReadStream(filename),
+        terminal: false
+    }).on('line', function(line) {
+        x += 1;
+        if (x == (parseInt(number[0]) + 3)) { 
+            var lineDeconstruct = line.split(" ")
+            console.log("wordle number: " + lineDeconstruct[4])
+            console.log("solution: " + lineDeconstruct[5])
+            const resolvedSolution = lineDeconstruct[5]
+            return resolvedSolution
+        }
+        
+    })
+
 
     let sortedArray = scores.filter(function (score) {
         return score.userId.slice(0,10) == userid
@@ -12,21 +33,17 @@ module.exports.run = async (bot, message, args) => {
         return score.wordleNumber == scoreid
     })
 
-
-
-    await getSolution.getSolution(args).then( solution => {
-        console.log("solution in get.js: " + solution)
-        if (sortedArray.length == 0) {
-            message.channel.send("No score found. Could be that you didn't play this day, or the number you entered was invalid.")
-        } else {
-            if (!solution) {
-                console.log("Solution not found")
-                solution = "solution not found"
-            }
-            message.channel.send(`Wordle ${scoreid} ${sortedArray[0].score}/6 ${sortedArray[0].scorePattern}
-            \nsolution: ||${solution}||`)
+    if (sortedArray.length == 0) {
+        message.channel.send("No score found. Could be that you didn't play this day, or the number you entered was invalid.")
+    } else {
+        if (!solution) {
+            console.log("Solution not found")
+            solution = "solution not found"
         }
-    })
+        message.channel.send(`Wordle ${scoreid} ${sortedArray[0].score}/6 ${sortedArray[0].scorePattern}
+        \nsolution: ||${solution}||`)
+    }
+
 
 }
 
