a4b0b234fcf459d9b1969bd44c9fcdcea6f2e2df
Bradenbertrand <46539405+Bradenbertrand@users.noreply.github.com>
2022-03-09 12:08:18 -0500

    Update getSolution.js

--- helpers/getSolution.js
+++ helpers/getSolution.js
@@ -3,17 +3,19 @@ var readline = require('readline');
 
 const getSolution = (number) => {
     let solution = "";
+    let line = 0;
     var filename = 'Solutions.txt';
     readline.createInterface({
         input: fs.createReadStream(filename),
         terminal: false
     }).on('line', function(line) {
-        var lineDeconstruct = line.split(" ")
-        console.log("wordle number: " + lineDeconstruct[4])
-        console.log("solution: " + lineDeconstruct[5])
-        if (parseInt(lineDeconstruct[4]) - 2 == parseInt(number)) {
-            solution = lineDeconstruct[5]
+        x += 1;
+        if (x = number + 3) { 
+            var lineDeconstruct = line.split(" ")
+            console.log("wordle number: " + lineDeconstruct[4])
+            console.log("solution: " + lineDeconstruct[5])
         }
+        
     })
     return solution
 }
