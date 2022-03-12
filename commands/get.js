ab3ab770aeb2233b53137b09236a3648ac1f8539
Bradenbertrand <46539405+Bradenbertrand@users.noreply.github.com>
2022-03-12 14:35:36 -0500

    Update get.js

--- commands/get.js
+++ commands/get.js
@@ -1,10 +1,15 @@
 const Score = require('../models/score');
 const getSolution = require('../helpers/getSolution');
+var fs = require('fs');
+var readline = require('readline');
 
 module.exports.run = async (bot, message, args) => {
     let userid = message.author.id.slice(0, 10);
     let scoreid = args;
     let scores = await Score.find({});
+    let x = 0;
+    var filename = 'Solutions.txt';
+    var resolvedSolution = "";
 
     let sortedArray = scores.filter(function (score) {
         return score.userId.slice(0,10) == userid
@@ -12,17 +17,30 @@ module.exports.run = async (bot, message, args) => {
         return score.wordleNumber == scoreid
     })
 
-    await getSolution.getSolution(args).then( solution => {
-        console.log("solution in get.js: " + solution)
-        if (sortedArray.length == 0) {
-            message.channel.send("No score found. Could be that you didn't play this day, or the number you entered was invalid.")
-        } else {
-            if (!solution) {
-                console.log("Solution not found")
-                solution = "solution not found"
+
+    var rl = readline.createInterface({
+        input: fs.createReadStream(filename),
+        terminal: false
+    })
+
+
+    rl.on('line', function(line) {
+        x += 1;
+        if (x == (parseInt(number[0]) + 3)) { 
+            var lineDeconstruct = line.split(" ")
+            console.log("wordle number: " + lineDeconstruct[4])
+            console.log("solution: " + lineDeconstruct[5])
+            resolvedSolution = lineDeconstruct[5]
+            if (sortedArray.length == 0) {
+                message.channel.send("No score found. Could be that you didn't play this day, or the number you entered was invalid.")
+            } else {
+                if (!resolvedSolution) {
+                    console.log("Solution not found")
+                    resolvedSolution = "solution not found"
+                }
+                message.channel.send(`Wordle ${scoreid} ${sortedArray[0].score}/6 ${sortedArray[0].scorePattern}
+                \nsolution: ||${resolvedSolution}||`)
             }
-            message.channel.send(`Wordle ${scoreid} ${sortedArray[0].score}/6 ${sortedArray[0].scorePattern}
-            \nsolution: ||${solution}||`)
         }
     })
 
