cfa51b086f6e745ad894c716f55dfb6dd690c89c
Bradenbertrand <46539405+Bradenbertrand@users.noreply.github.com>
2022-03-16 16:29:15 -0400

    Update top.js
    Fixed looking for users from server

--- commands/top.js
+++ commands/top.js
@@ -77,8 +77,8 @@ module.exports.run = async (bot, message, args) => {
         }
     } else if (args == "daily") {
         var todayDate = new Date();
-        let todaysScores = await Score.find({ servers: message.guild.id, date: { $gte: `${todayDate.getFullYear()}-${todayDate.getMonth()}-${todayDate.getDate()}`}}).sort({ score: 'asc' }).limit(5);
-        let users = await User.find({})
+        let todaysScores = await Score.find({ date: { $gte: `${todayDate.getFullYear()}-${todayDate.getMonth()}-${todayDate.getDate()}`}}).sort({ score: 'asc' }).limit(5);
+        let users = await User.find({ servers: message.guild.id })
         try {
             var i = 1;
             var returnedScores = `Top scores for today`;
