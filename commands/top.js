064bb9d48e165d18d42aec414c7e3147c3251d10
Bradenbertrand <46539405+Bradenbertrand@users.noreply.github.com>
2022-03-19 16:19:03 -0400

    Update top.js

--- commands/top.js
+++ commands/top.js
@@ -116,8 +116,12 @@ module.exports.run = async (bot, message, args) => {
                 let user = users.find(user => {
                     return score.userId.slice(0, 10) == user.userId.toString().slice(0, 10);
                 })
-                returnedScores += `#${i} - ${user.username} with a score of ${score.score}\n`;
-                i += 1
+                if (i > 5) {
+                    
+                } else {
+                    returnedScores += `#${i} - ${user.username} with a score of ${score.score}\n`;
+                    i += 1
+                }
             })
             message.channel.send(returnedScores);
         } catch (error) {
